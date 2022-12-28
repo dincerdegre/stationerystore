3f236d5f263349dc4f4ba3d2a66d3d26cc5b9cbc
dincerdegre <dincerdegre@gmail.com>
2022-12-28 22:27:57 +0300

    added router and navbar footer files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layouts/Layout";
+import Home from "./pages/Home/Home";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  }
+])
+
 function App() {
   return (
-    <div className="app">
-      
-    </div>
+    <RouterProvider router={router}/>
   );
 }
+
+
 
 export default App;

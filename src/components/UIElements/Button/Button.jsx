d8c12e907e95132411bee0adf42a6f409e412603
dincerdegre <dincerdegre@gmail.com>
2023-01-09 18:26:22 +0300

    Button component made

--- src/components/UIElements/Button/Button.jsx
+++ src/components/UIElements/Button/Button.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Button = () => {
+const Button = ({
+  href,
+  to,
+  onClick,
+  type,
+  disabled,
+  exact,
+  ariaLabel,
+  className,
+  children,
+}) => {
+  if (href) {
+    return (
+      <a className={className} href={href}>
+        {children}
+      </a>
+    );
+  }
+  if (to) {
+    return (
+      <Link to={to} exact={exact} className={className}>
+        {children}
+      </Link>
+    );
+  }
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      type={type}
+      onClick={onClick}
+      className={className}
+      aria-label={ariaLabel}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;

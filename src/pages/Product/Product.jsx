531e9a54a2699061f7fb484b2374bc62ae085d56
dincerdegre <dincerdegre@gmail.com>
2023-01-16 11:00:49 +0300

    Closed #18 elden geçirilip düzenlendi

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,33 +1,37 @@
-import React, { useState,useContext } from "react";
+import React, { useState, useContext } from "react";
 import { useParams } from "react-router-dom";
-import AddToCart from "../../components/AddToCart/AddToCart";
-import CountSelector from "../../components/CountSelector/CountSelector";
-import PriceItem from "../../components/PriceItem/PriceItem";
-import Loader from "../../components/UI/Loader/Loader";
+// import AddToCart from "../../components/AddToCart/AddToCart";
+// import CountSelector from "../../components/CountSelector/CountSelector";
+// import PriceItem from "../../components/PriceItem/PriceItem";
+import Loader from "../../components/UIElements/Loader/Loader";
 import productsJSON from "../../data/products.json";
 import CartContext from "../../context/cart-context";
-import "./Product.scss";
+import classes from "./Product.module.scss";
+import ImageItem from "../../components/Product/ImageItem/ImageItem";
+import AmountSelector from "../../components/shared/AmountSelector/AmountSelector";
+import AddToCart from "../../components/Product/AddToCart/AddToCart";
+import PriceItem from "../../components/Product/PriceItem/PriceItem";
 
 const Product = () => {
   const slug = useParams().slug;
   const [loading, setLoading] = useState(true);
-  const [selectedCount,setSelectedCount] = useState(1);
+  const [selectedAmount, setSelectedAmount] = useState(1);
   const [data, setData] = useState(null);
   const cartContext = useContext(CartContext);
 
   const addToCartHandler = () => {
     cartContext.addItem({
-      id:data.id,
-      title:data.title,
-      image:data.image,
-      price:data.price,
-      quantity:selectedCount
+      id: data.id,
+      title: data.title,
+      image: data.image,
+      price: data.price,
+      quantity: selectedAmount,
     });
-  }
+  };
 
-  const countChangeHandler = (quantity) => {
-    setSelectedCount(quantity);
-  }
+  const amountChangeHandler = (quantity) => {
+    setSelectedAmount(quantity);
+  };
 
   useState(() => {
     const timeout = setTimeout(() => {
@@ -43,46 +47,34 @@ const Product = () => {
     }, 500);
     return () => clearTimeout(timeout);
   }, [loading, data]);
-  
-    return loading ? (
-      <Loader />
-    ) : (
-      <div className="product">
-        <div className="productLeft">
-          <div className="mainImage">
-            <img src={data.image} alt="" />
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <div className={classes.productContainer}>
+      <div className={classes.left}>
+        <ImageItem title={data.title} image={data.image} />
+      </div>
+      <div className={classes.right}>
+        <div className={classes.title}>
+          <h2>{data.title}</h2>
+        </div>
+        <div className={classes.info}>
+          <div className={classes.priceInfo}>
+            <PriceItem price={data.price} oldPrice={data.oldprice} />
           </div>
-          <div className="otherImages">
-            <img src="/images/products/product20.jpg" alt="" />
-            <img src="/images/products/product19.jpg" alt="" />
-            <img src="/images/products/product18.jpg" alt="" />
-            <img src="/images/products/product18.jpg" alt="" />
-            <img src="/images/products/product18.jpg" alt="" />
+          <div className={classes.amountCartInfo}>
+            <AmountSelector onAmountChange={amountChangeHandler} size="large" />
+            <AddToCart onClick={addToCartHandler} />
           </div>
         </div>
-        <div className="productRight">
-          <div className="title">
-            <h2>{data.title}</h2>
-          </div>
-          <div className="info">
-            <div className="infoItem">
-              <PriceItem price={data.price} oldPrice={data.oldprice} />
-            </div>
-            <div className="infoItem">
-              <CountSelector onCounterChange={countChangeHandler} size="large" />
-              <AddToCart onClick={addToCartHandler} />
-            </div>
-          </div>
-          <div className="desc">
-            <h2>Product Details</h2>
-            <p>
-              {data.desc}
-            </p>
-          </div>
+        <div className={classes.desc}>
+          <h2>Product Details</h2>
+          <p>{data.desc}</p>
         </div>
       </div>
-    );
-  
+    </div>
+  );
 };
 
 export default Product;

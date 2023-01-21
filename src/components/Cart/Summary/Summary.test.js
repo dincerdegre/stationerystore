38962431cdd0ea1de7175a541b6040147b700f90
dincerdegre <dincerdegre@gmail.com>
2023-01-21 17:55:12 +0300

    Summary testi yapıldı

--- src/components/Cart/Summary/Summary.test.js
+++ src/components/Cart/Summary/Summary.test.js
@@ -11,4 +11,18 @@ describe("Summary Component", () => {
         const summarySubtotalEl = screen.getByText(/subtotal/i);
         expect(summarySubtotalEl).toBeInTheDocument();
     });
+
+    test('Render "Summary" component with All Parameters',()=>{
+        render(<Summary subtotal="10.00" tax="1.00" shipping="5.00" total="16.00" moneySymbol="TL" />);
+        const subtotalEl = screen.getByText("10.00",{exact:false});
+        expect(subtotalEl).toBeInTheDocument();
+        const taxEl = screen.getByText("1.00",{exact:false});
+        expect(taxEl).toBeInTheDocument();
+        const shippingEl = screen.getByText("5.00",{exact:false});
+        expect(shippingEl).toBeInTheDocument();
+        const totalEl = screen.getByText("16.00",{exact:false});
+        expect(totalEl).toBeInTheDocument();
+        const moneySymbolEl = screen.getAllByText("TL",{exact:false});
+        expect(moneySymbolEl.length).toBeGreaterThan(0);
+    });
 });

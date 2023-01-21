af338adc5c72aa7ae9a09f4046f06de82b6550e0
dincerdegre <dincerdegre@gmail.com>
2023-01-21 18:42:02 +0300

    Product pages navigate eklendi 404 page için

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,8 +1,5 @@
-import React, { useState, useContext } from "react";
-import { useParams } from "react-router-dom";
-// import AddToCart from "../../components/AddToCart/AddToCart";
-// import CountSelector from "../../components/CountSelector/CountSelector";
-// import PriceItem from "../../components/PriceItem/PriceItem";
+import React, { useState, useContext, Fragment } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import Loader from "../../components/UIElements/Loader/Loader";
 import productsJSON from "../../data/products.json";
 import CartContext from "../../context/cart-context";
@@ -18,6 +15,7 @@ const Product = () => {
   const [selectedAmount, setSelectedAmount] = useState(1);
   const [data, setData] = useState(null);
   const cartContext = useContext(CartContext);
+  const navigate = useNavigate();
 
   const addToCartHandler = () => {
     cartContext.addItem({
@@ -44,35 +42,46 @@ const Product = () => {
       } else {
         setLoading(true);
       }
+      if (dataJSON.length < 1) {
+        return navigate("/404", { replace: true });
+      }
     }, 500);
+
     return () => clearTimeout(timeout);
   }, [loading, data]);
 
-  return loading ? (
-    <Loader />
-  ) : (
+  return (
     <div className={classes.productContainer}>
-      <div className={classes.left}>
-        <ImageItem title={data.title} image={data.image} />
-      </div>
-      <div className={classes.right}>
-        <div className={classes.title}>
-          <h2>{data.title}</h2>
-        </div>
-        <div className={classes.info}>
-          <div className={classes.priceInfo}>
-            <PriceItem price={data.price} oldPrice={data.oldprice} />
+      {loading && !data ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <div className={classes.left}>
+            <ImageItem title={data?.title} image={data?.image} />
           </div>
-          <div className={classes.amountCartInfo}>
-            <AmountSelector onAmountChange={amountChangeHandler} size="large" />
-            <AddToCart onClick={addToCartHandler} />
+          <div className={classes.right}>
+            <div className={classes.title}>
+              <h2>{data?.title}</h2>
+            </div>
+            <div className={classes.info}>
+              <div className={classes.priceInfo}>
+                <PriceItem price={data?.price} oldPrice={data?.oldprice} />
+              </div>
+              <div className={classes.amountCartInfo}>
+                <AmountSelector
+                  onAmountChange={amountChangeHandler}
+                  size="large"
+                />
+                <AddToCart onClick={addToCartHandler} />
+              </div>
+            </div>
+            <div className={classes.desc}>
+              <h2>Product Details</h2>
+              <p>{data?.desc}</p>
+            </div>
           </div>
-        </div>
-        <div className={classes.desc}>
-          <h2>Product Details</h2>
-          <p>{data.desc}</p>
-        </div>
-      </div>
+        </Fragment>
+      )}
     </div>
   );
 };

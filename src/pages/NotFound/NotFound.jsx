cdf8cf2282534143a80684b7a46fac3ab00ff811
dincerdegre <dincerdegre@gmail.com>
2023-01-21 18:12:00 +0300

    Not Found Page eklendi

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './NotFound.module.scss';
+
+const NotFound = () => {
+  return (
+    <div className={classes.notFound}>404 Not Found</div>
+  )
+}
+
+export default NotFound

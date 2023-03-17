7fea0efa4ca02ec1407030ab7a1060c4083bd360
dincerdegre <dincerdegre@gmail.com>
2023-03-17 17:44:54 +0300

    deleted link

--- src/components/Layouts/Header/Header.jsx
+++ src/components/Layouts/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Button from '../../UIElements/Button/Button';
 import CartMenu from "../../shared/CartMenu/CartMenu";
 import HeaderRight from "../../shared/HeaderRight/HeaderRight";

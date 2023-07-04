d12f0238cb38541714765176a8c6152060580bcf
dincerdegre <dincerdegre@gmail.com>
2023-07-05 02:09:41 +0300

    button ui fix

--- src/components/UIElements/Button/Button.jsx
+++ src/components/UIElements/Button/Button.jsx
@@ -15,14 +15,14 @@ const Button = ({
 }) => {
   if (href) {
     return (
-      <a className={className} href={href} data-testid={dataTestid}>
+      <a className={className} href={href} onClick={onClick} data-testid={dataTestid}>
         {children}
       </a>
     );
   }
   if (to) {
     return (
-      <Link to={to} exact={exact} className={className} data-testid={dataTestid}>
+      <Link to={to} exact={exact} onClick={onClick} className={className} data-testid={dataTestid}>
         {children}
       </Link>
     );
